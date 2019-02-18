16f151e3bc0d763b31993fcfbeda87e2ac6f973a
Adam Nemecek <adamnemecek@gmail.com>
2019-02-17 16:04:01 -0800

    vector

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 
 
-pub struct Vec3 {
+use std::f32;
+use std::ops::Div;
 
+#[derive(Copy, Clone)]
+pub struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vec3 {
+    fn norm(self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    fn normalize(self) -> Self {
+        self / self.norm()
+    }
+}
+
+pub struct Mat3 {
+    r1: Vec3,
+    r2: Vec3,
+    r3: Vec3,
+}
+
+impl Div<f32> for Vec3{
+    type Output = Self;
+    fn div(self, other: f32) -> Self::Output {
+        Self::Output {x: self.x / other, y: self.y / other, z: self.z / other}
+    }
 }
 
 
