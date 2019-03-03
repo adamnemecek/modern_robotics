8b92d767b694124af6a473bc975c1cc2cf3fcb02
Adam Nemecek <adamnemecek@gmail.com>
2019-03-03 10:14:20 -0800

    add svd

---
+++ src/svd.rs
@@ -0,0 +1,20 @@
+
+
+// fn svd_1d()
+
+
+use crate::traits::{SquareMatrix, Shaped};
+
+pub struct SVD {
+
+}
+
+impl SVD {
+    pub fn new<A: SquareMatrix + Shaped>(a: A, k: Option<usize>) -> Self {
+        let shape = A::shape();
+
+        let k = k.unwrap_or(shape.0.min(shape.1));
+        
+        Self {}
+    }
+}
